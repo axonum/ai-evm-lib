a2cf60cbb0997636f2977f13ac5d6431b84333dd
axonon <anon@axonum.io>
2024-06-14 10:19:39 +0800

    feat: Update Solidity version constraint to allow up to 0.9.0

--- AILib.sol
+++ AILib.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.16 <=0.8.23;
+pragma solidity >=0.4.16 <0.9.0;
 
 library AILib {
     function inference(bytes32 model_address, bytes memory input_data, uint256 output_size) internal view returns (bytes memory) {
@@ -42,23 +42,22 @@ library AILib {
         return merged;
     }
 
-    function onnxInference(bytes memory model, bytes memory input_data) internal view returns (bytes memory) {
+    function onnxInference(bytes memory model, bytes memory input_data, uint256 output_size) internal view returns (bytes memory) {
         bytes memory input = mergeOnnxInput(model, input_data);
         uint256 len = input.length;
+        bytes memory output = new bytes(output_size);
+        uint256 retLen = 32 + output_size;
         assembly {
-            // call the precompiled contract onnxInference (0xa0)
-            let success := staticcall(gas(), 0xa0, add(input, 0x20), len, 0, 0)
+            // call the precompiled contract opmlInference (0xa1)
+            let success := staticcall(gas(), 0xa0, add(input, 0x20), len, output, retLen)
             
             // check if call was succesfull, else revert
             if iszero(success) {
                 revert(0,0)
             }
-
-            // store the return data in memory starting from the free memory pointer
-            returndatacopy(mload(0x40), 0 , returndatasize())
-            // return the result from memory
-            return(mload(0x40), returndatasize())
         }
+
+        return output;
     }
 
     function mergeOnnxInput(bytes memory model, bytes memory input_data) internal pure returns (bytes memory) {
--- OnnxContract.sol
+++ OnnxContract.sol
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.16 <=0.8.23;
+pragma solidity >=0.4.16 <0.9.0;
 
 import "./AILib.sol";
 
 contract OnnxContract {
     event OnnxInference(bytes model, bytes input, bytes output);
 
-    function onnxInference(bytes memory model, bytes memory input) public {
-        bytes memory output = AILib.onnxInference(model, input);
+    function onnxInference(bytes memory model, bytes memory input, uint256 output_size) public returns (bytes memory) {
+        bytes memory output = AILib.onnxInference(model, input, output_size);
         emit OnnxInference(model, input, output);
+        return output;
     }
 }
