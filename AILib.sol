5e53f376671255f3aa4b61529b5b9be12b26780e
axonon <167090211+axonon@users.noreply.github.com>
2024-10-01 16:06:04 +0800

    feat: onnx (#2)
    * feat(onnx): Add onnxInference function for AI inference using ONNX models
    
    * feat: Update Solidity version constraint to allow up to 0.9.0
    
    * chore: downgrade Solidity version

--- AILib.sol
+++ AILib.sol
@@ -41,4 +41,51 @@ library AILib {
 
         return merged;
     }
+
+    function onnxInference(bytes memory model, bytes memory input_data, uint256 output_size) internal view returns (bytes memory) {
+        bytes memory input = mergeOnnxInput(model, input_data);
+        uint256 len = input.length;
+        bytes memory output = new bytes(output_size);
+        uint256 retLen = 32 + output_size;
+        assembly {
+            // call the precompiled contract opmlInference (0xa1)
+            let success := staticcall(gas(), 0xa0, add(input, 0x20), len, output, retLen)
+            
+            // check if call was succesfull, else revert
+            if iszero(success) {
+                revert(0,0)
+            }
+        }
+
+        return output;
+    }
+
+    function mergeOnnxInput(bytes memory model, bytes memory input_data) internal pure returns (bytes memory) {
+        bytes32 model_size_bytes = bytes32(model.length);
+        bytes32 input_size_bytes = bytes32(input_data.length);
+        bytes memory merged = new bytes(model_size_bytes.length + input_size_bytes.length + model.length + input_data.length);
+
+        uint k = 0;
+        for (uint i = 0; i < model_size_bytes.length; i++) {
+            merged[k] = model_size_bytes[i];
+            ++k;
+        }
+
+        for (uint i = 0; i < input_size_bytes.length; i++) {
+            merged[k] = input_size_bytes[i];
+            ++k;
+        }
+
+        for (uint i = 0; i < model.length; i++) {
+            merged[k] = model[i];
+            ++k;
+        }
+
+        for (uint i = 0; i < input_data.length; i++) {
+            merged[k] = input_data[i];
+            ++k;
+        }
+
+        return merged;
+    }
 }
